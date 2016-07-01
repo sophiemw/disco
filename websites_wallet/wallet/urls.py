0f0ec4d3cbf43307ec2f6b0f9f4e8e299f7d7257
Sophie <sophie@mugridge.com>
2016-07-01 15:17:47 +0100

    User can log in and out

--- websites_wallet/wallet/urls.py
+++ websites_wallet/wallet/urls.py
@@ -6,7 +6,7 @@ app_name = 'wallet'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'signup/$', views.signup, name='signup'),
-    url(r'login/$', views.login, name='login'),
-    url(r'logout/$', views.logout, name='logout'),
+    url(r'login/$', views.userlogin, name='login'),
+    url(r'logout/$', views.userlogout, name='logout'),
     url(r'homepage/$', views.homepage, name='homepage'),
 ]
--- websites_wallet/wallet/views.py
+++ websites_wallet/wallet/views.py
@@ -1,7 +1,11 @@
 from django import forms
+from django.contrib import auth
+from django.contrib.auth import logout, authenticate, login
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.template import RequestContext
 
 
 from wallet.forms import SignupForm
@@ -31,8 +35,8 @@ def signup(request):
                 password=form.cleaned_data['password1'])
             new_user.save()
 
-            #if new_user is not None:
-            #    login(request, new_user)
+            if new_user is not None:
+                login(request, new_user)
 
             # redirect to a new URL:
             return HttpResponseRedirect('/wallet/')
@@ -43,11 +47,52 @@ def signup(request):
 
     return render(request, 'wallet/signup.html', {'form': form})
 
-def login(request):
-	return HttpResponse("This is the log in page")
+def userlogin(request):
+    # Like before, obtain the context for the user's request.
+    context = RequestContext(request)
 
-def logout(request):
-	return HttpResponse("This is the log out page")
+    # If the request is a HTTP POST, try to pull out the relevant information.
+    if request.method == 'POST':
+        # Gather the username and password provided by the user.
+        # This information is obtained from the login form.
+        username = request.POST['username']
+        password = request.POST['password']
 
+        # Use Django's machinery to attempt to see if the username/password
+        # combination is valid - a User object is returned if it is.
+        user = authenticate(username=username, password=password)
+
+        # If we have a User object, the details are correct.
+        # If None (Python's way of representing the absence of a value), no user
+        # with matching credentials was found.
+        if user:
+            # Is the account active? It could have been disabled.
+            if user.is_active:
+                # If the account is valid and active, we can log the user in.
+                # We'll send the user back to the homepage.
+                login(request, user)
+                return HttpResponseRedirect('/wallet/')
+            else:
+                # An inactive account was used - no logging in!
+                return HttpResponse("Your account is disabled.")
+        else:
+            # Bad login details were provided. So we can't log the user in.
+            print "Invalid login details: {0}, {1}".format(username, password)
+            return HttpResponse("Invalid login details supplied.")
+
+    # The request is not a HTTP POST, so display the login form.
+    # This scenario would most likely be a HTTP GET.
+    else:
+        # No context variables to pass to the template system, hence the
+        # blank dictionary object...
+        # http://stackoverflow.com/questions/21498682/django-csrf-verification-failed-request-aborted-csrf-cookie-not-set
+        return render(request, 'wallet/login.html', {})
+
+@login_required
+def userlogout(request):
+	logout(request)
+	return HttpResponseRedirect('/wallet/')
+
+@login_required
 def homepage(request):
-	return HttpResponse("This is the home page - when logged in (eventually)")
+	return HttpResponse("This is the home page - when logged in")
